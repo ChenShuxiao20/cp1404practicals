84f0416314d2267b69c46198ef5fc526b39a96a6
ChenShuxiao <shuxiao.chen@my.jcu.edu.au>
2024-05-30 16:06:21 +0800

    Print Score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,18 +1,13 @@
 # 3. Score
 """
 function main
-    score = get_a_valid_score()
+    get score
     grade = print_grade(score)
 
-function get_a_valid_score()
-    get score
-    while score < 0 or score > 100
-        print "Invalid score"
-        get score
-    return score
-
 function print_grade(score)
-    if score >= 50
+    if score < 0 or score > 100
+    print "Invalid score"
+    elif score >= 50
         print "Passable"
     elif score >= 90
         print "Excellent"
@@ -20,18 +15,13 @@ function print_grade(score)
         print "Bad"
 """
 def main():
-    score = get_a_valid_score()
+    score = float(input("Enter score: "))
     grade = print_grade(score)
 
-def get_a_valid_score():
-    score = float(input("Enter score: "))
-    while score < 0 or score > 100:
-        print("Invalid score")
-        score = float(input("Enter score: "))
-    return score
-
 def print_grade(score):
-    if score >= 50:
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 50:
         print("Passable")
     elif score >= 90:
         print("Excellent")
