abca775bad2cea541a73183e0d8cb75d44096aad
ChenShuxiao <shuxiao.chen@my.jcu.edu.au>
2024-05-30 15:58:25 +0800

    Score

---
+++ prac_02/score.py
@@ -0,0 +1,31 @@
+# 3. Score
+"""
+function main
+    score = float(input("Enter score: "))
+    grade = print_grade(score)
+
+function print_grade(score)
+    if score < 0 or score > 100
+        print "Invalid score"
+    elif score >= 50
+        print "Passable"
+    elif score >= 90
+        print "Excellent"
+    else
+        print "Bad"
+"""
+def main():
+    score = float(input("Enter score: "))
+    grade = print_grade(score)
+
+def print_grade(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 50:
+        print("Passable")
+    elif score >= 90:
+        print("Excellent")
+    else:
+        print("Bad")
+
+main()
