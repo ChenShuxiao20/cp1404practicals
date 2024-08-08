ec044fce34372bfabdc9a1430d266dc3ff044a43
ChenShuxiao <shuxiao.chen@my.jcu.edu.au>
2024-08-08 14:56:17 +0800

    Complete prac 10 testing exercise

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -7,6 +7,10 @@ import doctest
 from prac_06.car import Car
 
 
+import doctest
+from prac_06.car import Car
+
+
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
     return " ".join([s] * n)
